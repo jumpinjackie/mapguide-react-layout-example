6e4bfb17019d5b75eb3a318e708a9202b27b7ffa
Jackie Ng <jumpinjackie@gmail.com>
2017-05-12 02:46:20 +1000

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,35 @@
+/**
+ * main.tsx
+ *
+ * This is the primary entry point of your custom mapguide-react-layout viewer bundle
+ */
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { initDefaultCommands } from "mapguide-react-layout/lib/api/default-commands";
+import { ApplicationViewModel } from "mapguide-react-layout/lib/entries/application";
+import { AjaxViewerLayout } from "mapguide-react-layout/lib/layouts/ajax-viewer";
+import { SidebarLayout } from "mapguide-react-layout/lib/layouts/sidebar";
+import { AquaTemplateLayout } from "mapguide-react-layout/lib/layouts/aqua";
+import { TurquoiseYellowLayoutTemplate } from "mapguide-react-layout/lib/layouts/turquoise-yellow";
+import { LimeGoldLayoutTemplate } from "mapguide-react-layout/lib/layouts/limegold";
+import { SlateTemplateLayout } from "mapguide-react-layout/lib/layouts/slate";
+import { MaroonTemplateLayout } from "mapguide-react-layout/lib/layouts/maroon";
+import { registerLayout } from "mapguide-react-layout/lib/api/registry/layout";
+import { registerDefaultComponents } from "mapguide-react-layout/lib/api/default-components";
+import { bootstrap } from "mapguide-react-layout/lib/api/bootstrap";
+
+require("mapguide-react-layout/src/styles/index.css");
+
+bootstrap();
+registerLayout("ajax-viewer", () => <AjaxViewerLayout />);
+registerLayout("sidebar", () => <SidebarLayout />);
+registerLayout("aqua", () => <AquaTemplateLayout />);
+registerLayout("turquoise-yellow", () => <TurquoiseYellowLayoutTemplate />);
+registerLayout("limegold", () => <LimeGoldLayoutTemplate />);
+registerLayout("slate", () => <SlateTemplateLayout />);
+registerLayout("maroon", () => <MaroonTemplateLayout />);
+initDefaultCommands();
+registerDefaultComponents();
+
+export { ApplicationViewModel as Application };
---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+'use strict';
+const path = require('path');
+const webpack = require('webpack');
+const basePlugins = [
+    new webpack.DefinePlugin({
+        __DEV__: process.env.NODE_ENV !== 'production',
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+];
+const devPlugins = [
+    new webpack.NoEmitOnErrorsPlugin(),
+];
+const prodPlugins = [
+    new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: false,
+        options: {
+            context: __dirname
+        }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+        sourceMap: true,
+        compress: {
+            warnings: false,
+            screw_ie8: true
+        },
+        comments: false
+    })
+];
+const plugins = basePlugins
+    .concat(process.env.NODE_ENV === 'production' ? prodPlugins : [])
+    .concat(process.env.NODE_ENV === 'development' ? devPlugins : []);
+
+module.exports = {
+    entry: {
+        viewer: './src/main.tsx'
+    },
+    output: {
+        libraryTarget: "var",
+        library: "MapGuide",
+        path: path.join(__dirname, 'viewer/dist'),
+        //filename: '[name].[hash].js',
+        filename: '[name].js',
+        publicPath: '/',
+        //sourceMapFilename: '[name].[hash].js.map',
+        sourceMapFilename: '[name].js.map',
+        chunkFilename: '[id].chunk.js'
+    },
+    devtool: 'source-map',
+    resolve: {
+        alias: {},
+        modules: [
+            'node_modules'
+        ],
+        extensions: ['.webpack.js', '.web.js', '.tsx', '.ts', '.js']
+    },
+    plugins: plugins,
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /(node_modules|test-utils|\.test\.ts$)/
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.(woff|woff2|ttf|eot|svg)$/,
+                loader: "file-loader",
+                options: {
+                    name: "fonts/[name].[ext]",
+                    publicPath: "./dist/"
+                }
+            }
+        ]
+    }
+};
