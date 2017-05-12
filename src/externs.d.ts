f3d889ddfcc3d36580c2978c44907f98e08f5826
Jackie Ng <jumpinjackie@gmail.com>
2017-05-12 22:25:44 +1000

    - Stub proj4 external module - Document the entry point

---
+++ src/externs.d.ts
@@ -0,0 +1,8 @@
+/**
+ * externs.d.ts
+ *
+ * This file is for defining ad-hoc typings for libraries where we do not have type definitions for
+ * or the type definitions are of poor quality
+ */
+
+declare module "proj4";
--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,8 @@
  */
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import proj4 from "proj4";
 
-import { initDefaultCommands } from "mapguide-react-layout/lib/api/default-commands";
-import { ApplicationViewModel } from "mapguide-react-layout/lib/entries/application";
 import { AjaxViewerLayout } from "mapguide-react-layout/lib/layouts/ajax-viewer";
 import { SidebarLayout } from "mapguide-react-layout/lib/layouts/sidebar";
 import { AquaTemplateLayout } from "mapguide-react-layout/lib/layouts/aqua";
@@ -16,13 +15,20 @@ import { LimeGoldLayoutTemplate } from "mapguide-react-layout/lib/layouts/limego
 import { SlateTemplateLayout } from "mapguide-react-layout/lib/layouts/slate";
 import { MaroonTemplateLayout } from "mapguide-react-layout/lib/layouts/maroon";
 import { SampleLayoutTemplate } from "./templates/SampleTemplate";
+
+import { initDefaultCommands } from "mapguide-react-layout/lib/api/default-commands";
+import { ApplicationViewModel } from "mapguide-react-layout/lib/entries/application";
 import { registerLayout } from "mapguide-react-layout/lib/api/registry/layout";
 import { registerDefaultComponents } from "mapguide-react-layout/lib/api/default-components";
 import { bootstrap } from "mapguide-react-layout/lib/api/bootstrap";
 
+// This will pull in and embed the core stylesheet into the viewer bundle
 require("mapguide-react-layout/src/styles/index.css");
 
+// Sets up the required core libraries
 bootstrap();
+
+// Register the templates we intend to provide in this viewer. Below is the standard set
 registerLayout("ajax-viewer", () => <AjaxViewerLayout />);
 registerLayout("sidebar", () => <SidebarLayout />);
 registerLayout("aqua", () => <AquaTemplateLayout />);
@@ -30,8 +36,22 @@ registerLayout("turquoise-yellow", () => <TurquoiseYellowLayoutTemplate />);
 registerLayout("limegold", () => <LimeGoldLayoutTemplate />);
 registerLayout("slate", () => <SlateTemplateLayout />);
 registerLayout("maroon", () => <MaroonTemplateLayout />);
+
+// Register our custom viewer template
 registerLayout("sample-template", () => <SampleLayoutTemplate />);
+
+// Register the default set of commands (zoom/pan/etc/etc) to the command registry
 initDefaultCommands();
+
+// Register the default set of components
 registerDefaultComponents();
 
+// Export external libraies under the MapGuide.Externals namespace
+export const Externals = {
+    proj4: proj4,
+    React: React,
+    ReactDOM: ReactDOM
+};
+
+// Export the MapGuide.Application entry point class
 export { ApplicationViewModel as Application };
