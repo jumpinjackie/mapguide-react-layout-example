9cd1f523949c6bebaeceac8c5d93ce9aa99f6232
Jackie Ng <jumpinjackie@gmail.com>
2017-05-13 00:20:04 +1000

    More commentary

--- src/templates/SampleTemplate.tsx
+++ src/templates/SampleTemplate.tsx
@@ -12,21 +12,58 @@ import { ModalLauncher } from "mapguide-react-layout/lib/containers/modal-launch
 const SIDEBAR_WIDTH = 250;
 const TASKPANE_HEIGHT = 500;
 
+/**
+ * This interface defines any props that can be passed into the component
+ *
+ * @export
+ * @interface ISampleLayoutTemplateProps
+ */
 export interface ISampleLayoutTemplateProps {
 
 }
 
+/**
+ * This interface defines observable redux state that is automatically updated when new state is
+ * dispatched to the redux store. This is how the component can automatically render/update based
+ * on change of state in the redux store.
+ *
+ * The function mapStateToProps is used to project the redux state tree to the properties defined
+ * in this interface
+ *
+ * @export
+ * @interface ISampleLayoutTemplateState
+ */
 export interface ISampleLayoutTemplateState {
     isBusy: boolean;
     locale: string;
 }
 
+/**
+ * This interfaces defines actions that can push new state to the redux store
+ *
+ * The function mapDispatchToProps is used to map the actions to invoke the redux
+ * dispatcher that is passed
+ *
+ * @export
+ * @interface ISampleLayoutTemplateDispatch
+ */
 export interface ISampleLayoutTemplateDispatch {
 
 }
 
+/**
+ * The react component consists of props and state. As the above interface are all part of the component's props, this type alias
+ * is a convenient descriptor of the props as a whole, an intersection type of the above interfaces. The Partial<> type wrapper
+ * means that all members of this intersection type are optional.
+ */
 export type SampleLayoutTemplateProps = Partial<ISampleLayoutTemplateProps & ISampleLayoutTemplateState & ISampleLayoutTemplateDispatch>;
 
+/**
+ * This function projects parts of the redux state tree into the target properties of our component state interface
+ *
+ * @param {IApplicationState} state The redux state tree
+ * @returns {Partial<ISampleLayoutTemplateState>}
+ */
 function mapStateToProps(state: IApplicationState) : Partial<ISampleLayoutTemplateState> {
     return {
         isBusy: state.viewer.busyCount > 0,
@@ -34,36 +71,71 @@ function mapStateToProps(state: IApplicationState) : Partial<ISampleLayoutTempla
     };
 }
 
+/**
+ * This function maps actions of our component dispatch interface to calls to the provided
+ * redux action dispatcher function.
+ *
+ * As this component does not push any state, this interface has no actions and thus this
+ * function returns an empty object
+ *
+ * @param {ReduxDispatch} dispatch
+ * @returns {ISampleLayoutTemplateDispatch}
+ */
 function mapDispatchToProps(dispatch: ReduxDispatch): ISampleLayoutTemplateDispatch {
     return {
 
     };
 }
 
+/**
+ * This react component represents our example viewer template. It subscribes to state in
+ * the redux store via the connect() decorator function
+ *
+ * @export
+ * @class SampleLayoutTemplate
+ * @extends {React.Component<SampleLayoutTemplateProps, any>}
+ */
 @connect(mapStateToProps, mapDispatchToProps)
 export class SampleLayoutTemplate extends React.Component<SampleLayoutTemplateProps, any> {
     constructor(props: SampleLayoutTemplateProps) {
         super(props);
     }
-    render(): JSX.Element  | null {
+    render(): JSX.Element | null {
         const { locale, isBusy } = this.props;
         return <div style={{ width: "100%", height: "100%" }}>
             <div style={{ position: "absolute", left: 0, top: 0, bottom: 0, width: SIDEBAR_WIDTH }}>
                 <div style={{ position: "absolute", left: 0, top: 0, right: 0, bottom: TASKPANE_HEIGHT, overflowY: "auto" }}>
+                    {/* This is the legend component */}
                     <PlaceholderComponent id={DefaultComponentNames.Legend} locale={locale} componentProps={{ inlineBaseLayerSwitcher: true }} />
                 </div>
                 <div style={{ position: "absolute", left: 0, right: 0, bottom: 0, height: TASKPANE_HEIGHT }}>
+                    {/* This is the task pane component */}
                     <PlaceholderComponent id={DefaultComponentNames.TaskPane} locale={locale} />
                 </div>
             </div>
+            {/* This is the primary toolbar */}
             <ToolbarContainer id={Constants.WEBLAYOUT_TOOLBAR} containerStyle={{ position: "absolute", left: SIDEBAR_WIDTH, top: 0, zIndex: 100, backgroundColor: TOOLBAR_BACKGROUND_COLOR }} />
             <div style={{ position: "absolute", left: SIDEBAR_WIDTH, top: DEFAULT_TOOLBAR_SIZE, bottom: 0, right: 0 }}>
+                {/* The map component */}
                 <PlaceholderComponent id={DefaultComponentNames.Map} locale={locale} />
+                {/* The navigator (zoom slider) component */}
                 <PlaceholderComponent id={DefaultComponentNames.Navigator} locale={locale} />
+                {/* The mouse coordinates display component */}
                 <PlaceholderComponent id={DefaultComponentNames.MouseCoordinates} locale={locale} />
+                {/* The scale display component */}
                 <PlaceholderComponent id={DefaultComponentNames.ScaleDisplay} locale={locale} />
+                {/*
+                This component automatically installs polyfill APIs for the AJAX viewer and partial support for the Fusion API, such APIs
+                are accessible from Task Pane content.
+                 */}
                 <ViewerApiShim />
+                {/*
+                This component services modal content display requests and displays such content in floating modal dialogs when requested
+                */}
                 <ModalLauncher />
+                {/*
+                This component services request for displaying flyout menus
+                */}
                 <FlyoutRegionContainer />
             </div>
         </div>
