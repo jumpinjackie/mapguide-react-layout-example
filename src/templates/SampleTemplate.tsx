f513c6f9f0b626116ff410bc180ad75153f739da
Jackie Ng <jumpinjackie@gmail.com>
2017-05-12 20:20:44 +1000

    Hook up the SampleLayoutTemplate to the redux store.

--- src/templates/SampleTemplate.tsx
+++ src/templates/SampleTemplate.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import { connect } from "react-redux";
 import { PlaceholderComponent, DefaultComponentNames } from "mapguide-react-layout/lib/api/registry/component";
+import { IApplicationState, ReduxDispatch } from "mapguide-react-layout/lib/api/common";
 
 export interface ISampleLayoutTemplateProps {
     
 }
 
 export interface ISampleLayoutTemplateState {
+    isBusy: boolean;
     locale: string;
 }
 
@@ -16,12 +18,26 @@ export interface ISampleLayoutTemplateDispatch {
 
 export type SampleLayoutTemplateProps = Partial<ISampleLayoutTemplateProps & ISampleLayoutTemplateState & ISampleLayoutTemplateDispatch>;
 
-export class SampleLayoutTemplate extends React.Component<SampleLayoutTemplateProps, object> {
+function mapStateToProps(state: IApplicationState) : Partial<ISampleLayoutTemplateState> {
+    return {
+        isBusy: state.viewer.busyCount > 0,
+        locale: state.config.locale
+    };
+}
+
+function mapDispatchToProps(dispatch: ReduxDispatch): ISampleLayoutTemplateDispatch {
+    return {
+
+    };
+}
+
+@connect(mapStateToProps, mapDispatchToProps)
+export class SampleLayoutTemplate extends React.Component<SampleLayoutTemplateProps, any> {
     constructor(props: SampleLayoutTemplateProps) {
         super(props);
     }
-    render(): JSX.Element {
-        const { locale } = this.props;
+    render(): JSX.Element  | null {
+        const { locale, isBusy } = this.props;
         return <div style={{ width: "100%", height: "100%" }}>
             <PlaceholderComponent id={DefaultComponentNames.Map} locale={locale} />;
         </div>;
